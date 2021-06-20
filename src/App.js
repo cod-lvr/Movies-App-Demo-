0df0a1e951ed84c440f99460a9f85466e8ac0a5e
cod-lvr <aymang29@yahoo.com>
2021-06-21 00:53:12 +0200

    description

--- src/App.js
+++ src/App.js
@@ -1,8 +1,72 @@
+import React, { Fragment, useCallback, useEffect, useState } from "react";
+import MoviesList from "./components/MoviesList";
+import "./App.css";
+import AddMovie from "./components/AddMovie";
+
 function App() {
+  const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const fetchingDataHandler = useCallback(() => {
+    setIsLoading(true);
+    setError(null);
+
+    fetch("https://swapi.dev/api/films/")
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Something went wrong!");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        const translatedData = data.results.map((moviedata) => {
+          return {
+            id: moviedata.episode_id,
+            title: moviedata.title,
+            openingText: moviedata.opening_crawl,
+            releaseDate: moviedata.release_date,
+          };
+        });
+        setMovies(translatedData);
+        setIsLoading(false);
+      })
+      .catch((err) => {
+        setError(err.message);
+        setIsLoading(false);
+      });
+  }, []);
+
+  useEffect(() => {
+    fetchingDataHandler();
+  }, [fetchingDataHandler]);
+
+  const addMovieHandler = (movie) => {
+    console.log(movie);
+  };
+
+  let content = <p>found no movies.</p>;
+
+  if (movies.length > 0) {
+    content = <MoviesList movies={movies} />;
+  }
+  if (error) {
+    content = <p>{error}</p>;
+  }
+  if (isLoading) {
+    content = <p>Loading...</p>;
+  }
+
   return (
-    <div>
-      <h2>Let's get started!</h2>
-    </div>
+    <Fragment>
+      <section>
+        <AddMovie onAddMovie={addMovieHandler} />
+      </section>
+      <section>
+        <button onClick={fetchingDataHandler}>Fetch new movie</button>
+      </section>
+      <section>{content}</section>
+    </Fragment>
   );
 }
 
